d138a35e8d2436d11b3c220c198e87f580368054
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-07-07 01:21:45 -0300

    feat: ParticlesComponent with particlets

---
+++ src/components/ParticlesComponent.tsx
@@ -0,0 +1,70 @@
+import { useCallback } from "react";
+import type { Container, Engine, ISourceOptions } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+export default function ParticlesComponent() {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+
+    const options: ISourceOptions = {
+                background: {
+                    color: {
+                        value: "#000",
+                    },
+                },
+                fpsLimit: 120,
+                smooth:true,
+                fullScreen: {
+                    enable: true,
+                    zIndex: -1
+                },
+                interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "grab",
+                        },
+                        resize: true,
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#444",
+                    }, 
+                    move: {
+                        direction: "bottom",
+                        enable: true,
+                        speed: 0.4,
+                    },
+                    number: {
+                        density:{
+                            enable:true,
+                            area: 100
+                        },
+                        value: 15,
+                    },
+                    opacity: {
+                        value: 1,
+                    },
+                    size: {
+                        value: 0.7,
+                    },
+                },
+                detectRetina: true,
+            }
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={options}
+        />
+    );
+};
