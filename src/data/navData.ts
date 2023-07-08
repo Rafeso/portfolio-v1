b2977aed48de9589de553f00e54fa0c57af13cc0
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-07-08 07:15:30 -0300

    feat: NavBar component

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -1,6 +1,12 @@
 const navData = [
-	{ name: 'Projetos', href: '/projects' },
-	{ name: 'Sobre mim', href: '/about' }
+	{ 
+		name: 'Projetos', 
+		href: '/projects' 
+	},
+	{ 
+		name: 'Sobre mim', 
+		href: '/about' 
+	}
 ];
 
 export default navData
---
+++ src/data/socialData.ts
@@ -0,0 +1,12 @@
+const socialData = [
+	{ 
+    name: 'GitHub', 
+    href: 'https://github.com/Rafeso' 
+  },
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/rafael-feitosa-souza/'
+	}
+];
+
+export default socialData
