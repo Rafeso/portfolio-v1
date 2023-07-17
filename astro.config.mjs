935ae936ca39c4917b362a51627f8c1fd031baf6
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-07-16 23:02:16 -0300

    feat: prefetch pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), compress()],
+  site: 'https://rafael-feitosa.vercel.app',
+  integrations: [tailwind(), react(), compress(), sitemap(), prefetch(), robotsTxt()],
   experimental: {
     assets: true
   }
