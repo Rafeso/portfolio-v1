666a346b77e8c2712fc7f2046447fb9b1d2923eb
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-07-16 23:30:24 -0300

    chore: vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import react from "@astrojs/react";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+import vercel from '@astrojs/vercel/edge';
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rafael-feitosa.vercel.app',
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
   integrations: [tailwind(), react(), compress(), sitemap(), prefetch(), robotsTxt()],
   experimental: {
     assets: true
